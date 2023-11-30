01685c90ef9b969609ab16c2bdf845fb49ef9436
Brian Postlethwaite <brian_pos@hotmail.com>
2023-11-30 15:02:25 +1100

    Refactor the VersionAgnosticSearchParameter out into its own file

--- src/Hl7.Fhir.Base.FhirPath.Validator/SearchExpressionValidator.cs
+++ src/Hl7.Fhir.Base.FhirPath.Validator/SearchExpressionValidator.cs
@@ -10,45 +10,6 @@ using System.Linq;
 namespace Hl7.Fhir.FhirPath.Validator
 {
     /// <summary>
-    /// Version Agnostic implementation of the SearchParameter class from the various versions of FHIR (although isn't FIHR and hasn't changed)
-    /// </summary>
-    [System.Diagnostics.DebuggerDisplay(@"\{{DebuggerDisplay,nq}}")] // http://blogs.msdn.com/b/jaredpar/archive/2011/03/18/debuggerdisplay-attribute-best-practices.aspx
-    public class VersionAgnosticSearchParameter
-    {
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        private string DebuggerDisplay
-        {
-            get
-            {
-                return String.Format("{0} {1} {2} ({3})", Resource, Name, Type, Expression);
-            }
-        }
-
-        public string Resource { get; set; }
-        public string Name { get; set; }
-        public string Code { get; set; }
-        public string Url { get; set; }
-        public SearchParamType Type { get; set; }
-
-        /// <summary>
-        /// The FHIR Path expresssion that can be used to extract the data
-        /// for this search parameter
-        /// </summary>
-        public string Expression { get; set; }
-
-        /// <summary>
-        /// If this is a reference, the possible types of resources that the
-        /// parameters references to
-        /// </summary>
-        public string[] Target { get; set; }
-
-        /// <summary>
-        /// Used to define the parts of a composite search parameter.
-        /// </summary>
-        public SearchParamComponent[] Component { get; set; }
-    }
-
-    /// <summary>
     /// Version Agnostic implementation used by the above VersionAgnosticSearchParameter class
     /// </summary>
     public struct SearchParamComponent
