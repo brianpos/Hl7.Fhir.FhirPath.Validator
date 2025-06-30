39a3e1f2a914131090c3eae7b35263f0ca46bab5
Brian Postlethwaite <brian_pos@hotmail.com>
2025-06-30 16:34:44 +1000

    Include static type checking for the HL7 core unit tests

---
+++ src/Test.Fhir.R5.FhirPath.Validator/Hl7UnitTests.cs
@@ -0,0 +1,196 @@
+﻿using Hl7.Fhir.FhirPath.Validator;
+using Hl7.FhirPath.Expressions;
+using Hl7.FhirPath;
+using Hl7.Fhir.Serialization;
+using Hl7.Fhir.Model;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Xml.Serialization;
+using Test.Fhir.R4B.FhirPath.Validator;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hl7.Fhir.Rest;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+using System.Linq.Expressions;
+using System.CodeDom.Compiler;
+
+namespace Test.Fhir.FhirPath.Validator
+{
+	/// <summary>
+	/// Run all the tests from here (R4B) https://github.com/FHIR/fhir-test-cases
+	/// </summary>
+	[TestClass]
+	public class Hl7UnitTestFileR5
+	{
+		FhirPathCompiler _compiler;
+		static Dictionary<string, TestData> _testData;
+		static Dictionary<string, Resource> _resources = new Dictionary<string, Resource>();
+
+		[TestInitialize]
+		public void Init()
+		{
+			Hl7.Fhir.FhirPath.ElementNavFhirExtensions.PrepareFhirSymbolTableFunctions();
+			SymbolTable symbolTable = new(FhirPathCompiler.DefaultSymbolTable);
+			_compiler = new FhirPathCompiler(symbolTable);
+
+			TestDataKeys.Count(); // force the test data to be loaded
+		}
+
+		public class TestData
+		{
+			public string groupName;
+			public string testName;
+			public string resourceType;
+			public string expression;
+			public string expressionValid;
+			public string outputType;
+			public bool emptyOutput;
+			public Resource resource;
+		}
+
+		public static IEnumerable<object[]> TestDataKeys
+		{
+			get
+			{
+				if (_testData == null)
+				{
+					_testData = new Dictionary<string, TestData>();
+					foreach (var inputData in ExpressionsInTests)
+					{
+						_testData.Add($"{inputData.groupName}.{inputData.testName}", inputData);
+					}
+				}
+				foreach (var key in _testData.Keys)
+				{
+					var data = _testData[key];
+					yield return new object[] { data.groupName, data.testName };
+				}
+			}
+		}
+
+		public static IEnumerable<TestData> ExpressionsInTests
+		{
+			get
+			{
+				var result = new List<TestData>();
+				string testFileXml = @"c:\git\hl7\fhir-test-cases\r5\fhirpath\tests-fhir-r5.xml";
+				string testBasePath = @"c:\git\hl7\fhir-test-cases\r5\";
+				XmlSerializer serializer = new XmlSerializer(typeof(Tests));
+				var jsonSettings = new FhirJsonPocoDeserializerSettings() { Validator = null, AnnotateResourceParseExceptions = true, ValidateOnFailedParse = false };
+				var jsonDS = new FhirJsonPocoDeserializer(jsonSettings);
+				var xmlSettings = new FhirXmlPocoDeserializerSettings() { Validator = null, AnnotateResourceParseExceptions = true, ValidateOnFailedParse = false };
+				var xmlDS = new FhirXmlPocoDeserializer(xmlSettings);
+
+				using (StreamReader reader = new StreamReader(testFileXml))
+				{
+					Tests tests = (Tests)serializer.Deserialize(reader);
+					//Console.WriteLine($"Name: {tests.Name}");
+					//Console.WriteLine($"{tests.Description}");
+					//Console.WriteLine("-----------------------------------");
+
+					// Now run through these tests
+					foreach (var g in tests.Groups)
+					{
+						//Console.WriteLine($"Group: {g.Name} {g.Description}");
+						//Console.WriteLine("-----------------------------------");
+						foreach (var t in g.Tests)
+						{
+							if (t.Mode == "cda") 
+								continue; // not testing CDA documents
+
+							// Now parse in the test file
+							string content = null;
+							if (!string.IsNullOrEmpty(t.InputFile))
+							{
+								if (File.Exists(Path.Combine(testBasePath, t.InputFile)))
+									content = System.IO.File.ReadAllText(Path.Combine(testBasePath, t.InputFile));
+								else if (File.Exists(Path.Combine(testBasePath, "examples", t.InputFile)))
+									content = System.IO.File.ReadAllText(Path.Combine(testBasePath, "examples", t.InputFile));
+								else
+									content = "??????";
+							}
+							else
+								content = "<Patient xmlns=\"http://hl7.org/fhir\"><id value=\"pat1\"/></Patient>";
+							Resource r = null;
+							if (t.InputFile?.EndsWith("json") == true)
+								r = jsonDS.DeserializeResource(content);
+							else
+								r = xmlDS.DeserializeResource(content);
+
+							var expression = t.Expression.Text;
+							//if (expression.Contains("//") || expression.Contains("/*") && expression.Contains("*/"))
+							//	continue;
+
+							// Perform the static analysis on the expression!
+							result.Add(new TestData
+							{
+								groupName = g.Name,
+								testName = t.Name,
+								resourceType = r.TypeName,
+								expression = t.Expression.Text,
+								expressionValid = t.Expression.Invalid,
+								emptyOutput = t.Outputs?.Count == 0,
+								outputType = t.Outputs?.FirstOrDefault()?.Type,
+								resource = r
+							});
+						}
+					}
+				}
+				return result;
+			}
+		}
+
+
+		[TestMethod]
+		[DynamicData(nameof(TestDataKeys))]
+		public void CheckStaticReturnTypes(string groupName, string testName)
+		{
+			var testData = _testData[$"{groupName}.{testName}"];
+
+			// string expression = "(software.empty() and implementation.empty()) or kind != 'requirements'";
+			Console.WriteLine($"{groupName} - {testName}");
+			Console.WriteLine($"Resource Type: {testData.resourceType}");
+			Console.WriteLine($"Expression:\r\n{testData.expression}");
+			Console.WriteLine("---------");
+			var visitor = new FhirPathExpressionVisitor();
+			visitor.SetContext(testData.resourceType);
+			try
+			{
+				var pe = _compiler.Parse(testData.expression);
+				var r = pe.Accept(visitor);
+				Console.WriteLine($"Result: {r}");
+				Console.WriteLine("---------");
+
+				Console.WriteLine(visitor.ToString());
+				Console.WriteLine(visitor.Outcome.ToXml(new FhirXmlSerializationSettings() { Pretty = true }));
+				if (testData.expressionValid == "semantic" || testData.expressionValid == "execution")
+				{
+					if (visitor.Outcome.Success)
+					{
+						Assert.IsFalse(r.Types.Any());
+					}
+					// Assert.IsFalse(visitor.Outcome.Success);
+				}
+				else
+					Assert.IsTrue(visitor.Outcome.Success, $"expected: {testData.expressionValid}");
+
+				if (string.IsNullOrEmpty(testData.expressionValid) && string.IsNullOrEmpty(testData.outputType) && !string.IsNullOrEmpty(r.ToString()))
+				{
+					if (!testData.emptyOutput)
+					{
+						Assert.Inconclusive($"Test did not have an output type defined, result returned {r}");
+						Assert.AreEqual(testData.outputType, r.ToString());
+					}
+				}
+			}
+			catch (FormatException ex)
+			{
+				Assert.IsTrue(testData.expressionValid == "syntax", $"unexpected compilation error: {ex.Message}");
+			}
+		}
+	}
+}
---
+++ src/Test.FhirPath.Validator/TestFileDefinition.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Test.Fhir.R4B.FhirPath.Validator
+{
+    [XmlRoot(ElementName = "tests")]
+    public class Tests
+    {
+        public string Notes { get; set; }
+
+        [XmlElement(ElementName = "group")]
+        public List<Group> Groups { get; set; }
+
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+
+		[XmlAttribute(AttributeName = "mode")]
+		public string Mode { get; set; }
+
+		[XmlAttribute(AttributeName = "description")]
+        public string Description { get; set; }
+
+        [XmlAttribute(AttributeName = "reference")]
+        public string Reference { get; set; }
+    }
+
+    public class Group
+    {
+        public string Notes { get; set; }
+
+        [XmlElement(ElementName = "test")]
+        public List<Test> Tests { get; set; }
+
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+
+        [XmlAttribute(AttributeName = "description")]
+        public string Description { get; set; }
+
+        [XmlAttribute(AttributeName = "reference")]
+        public string Reference { get; set; }
+    }
+
+    public class Test
+    {
+        [XmlElement(ElementName = "expression")]
+        public Expression Expression { get; set; }
+
+        [XmlElement(ElementName = "output")]
+        public List<Output> Outputs { get; set; }
+
+        public string Notes { get; set; }
+
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+
+        [XmlAttribute(AttributeName = "description")]
+        public string Description { get; set; }
+
+        [XmlAttribute(AttributeName = "reference")]
+        public string Reference { get; set; }
+
+        [XmlAttribute(AttributeName = "inputfile")]
+        public string InputFile { get; set; }
+
+        [XmlAttribute(AttributeName = "mode")]
+        public string Mode { get; set; }
+
+        [XmlAttribute(AttributeName = "checkOrderedFunctions")]
+        public bool CheckOrderedFunctions { get; set; }
+
+        [XmlAttribute(AttributeName = "predicate")]
+        public bool Predicate { get; set; }
+    }
+
+    public class Expression
+    {
+        [XmlAttribute(AttributeName = "invalid")]
+        public string Invalid { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    public class Output
+    {
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    public enum OutputType
+    {
+        Boolean,
+        Code,
+        Date,
+        DateTime,
+        Decimal,
+        Integer,
+        Quantity,
+        String,
+        Time
+    }
+
+    public enum InvalidType
+    {
+        [XmlEnum("false")]
+        False,
+        Semantic,
+        [XmlEnum("true")]
+        True
+    }
+}
